891c086452f01729875bd957acc6481f0d8d7d27
Homer <45139235+buckhorn17@users.noreply.github.com>
2023-06-08 10:20:04 +0800

    version01

---
+++ all.js
@@ -0,0 +1,35 @@
+$(document).ready(function () {
+  
+    $('.hamBar-menu').click(function(){
+      $(".menu").slideToggle(400);
+      $(".menu").css("display","flex");
+      $(".hamBar-menu").css("display","none");
+      $(".hamBar-close").css("display","flex");
+      })
+
+    $('.hamBar-close').click(function(){
+      $(".menu").slideToggle(400);
+      $(".menu").css("display","flex");
+      $(".hamBar-menu").css("display","flex");
+      $(".hamBar-close").css("display","none");
+    })
+
+    $('.udrt').click(function(){
+      $("html,body").animate({scrollTop: 0},500)
+      })
+
+
+      // $('.card').hover(function(){
+      //   $("img").addClass('imgBig');
+        
+      //   })
+
+
+    // $('.deco').ready(function(){
+    //   $("img").animate({top:'200px'});
+
+    // })
+
+
+      
+});
